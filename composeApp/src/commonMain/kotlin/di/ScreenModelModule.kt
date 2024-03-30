5117c61ee0803dacb4c001ece81ff3371e6fe58b
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-03-30 06:38:29 +0200

    finish order screen

--- composeApp/src/commonMain/kotlin/di/ScreenModelModule.kt
+++ composeApp/src/commonMain/kotlin/di/ScreenModelModule.kt
@@ -5,20 +5,15 @@ import org.koin.dsl.module
 import presentation.app.AppScreenModel
 import presentation.screen.dinin.DinInScreenModel
 import presentation.screen.home.HomeScreenModel
-import presentation.screen.order.modifiers.ItemModifierState
-import presentation.screen.order.modifiers.ItemModifiersListScreenModel
+import presentation.screen.order.OrderScreenModel
 import presentation.screen.setting.SettingScreenModel
 
 val screenModelModule = module {
     factoryOf(::AppScreenModel)
     factoryOf(::HomeScreenModel)
     factoryOf(::DinInScreenModel)
-    factoryOf(::PresetsListScreenModel)
     factoryOf(::SettingScreenModel)
-    factory { (presetId: Int) ->
-        ItemsListScreenModel(get(), presetId)
-    }
-    factory { (items: List<ItemModifierState>) ->
-        ItemModifiersListScreenModel(items)
+    factory { (checkId: Int) ->
+        OrderScreenModel(get(), checkId)
     }
 }
